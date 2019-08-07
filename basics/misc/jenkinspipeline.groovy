5bbdf8a7f1e7e5c97e05c6c094dacc580c14d195
shirlauterbach <shir.lauterbach@gmail.com>
2019-08-07 15:25:16 +0300

    Adding jenkinsfile2 reference

--- basics/misc/jenkinspipeline.groovy
+++ basics/misc/jenkinspipeline.groovy
@@ -20,3 +20,26 @@ pipelineJob('pipeline') {
     }
   }
 }
+
+pipelineJob('pipeline-jenkins2') {
+
+  def repo = 'https://github.com/shirlauterbach/docker-cicd.git'
+
+  triggers {
+    scm('H/5 * * * *')
+  }
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master', '**/feature*')
+          scriptPath('./basics/misc/Jenkinsfile.v2')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+      }
+    }
+  }
+}
